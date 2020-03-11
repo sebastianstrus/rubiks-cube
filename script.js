a1ad3d6051a0124a25ac71fa6c8589c9b0b164f5
numi3257 <numi3257@gmail.com>
2020-03-11 20:35:12 +0100

    Added new version

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ var allRotations = ["L", "F", "R", "B", "U", "D", "L'", "F'", "R'", "B'", "U'", 
 
 var algorithm = document.getElementById("algorithm");
 
+var isPlaying = false;
+
+var isSecretHidden = true;
+var secretNumber = 1;
+var secretMaxNumber = 14;
+
+
 var l1 = document.getElementById("l1")
 var l2 = document.getElementById("l2")
 var l3 = document.getElementById("l3")
@@ -137,6 +144,8 @@ function lClicked() {
     b9.style.backgroundColor = d1color;
 
 
+
+    checkResult();
 }
 
 function lReverseClicked() {
@@ -189,6 +198,7 @@ function lReverseClicked() {
     d7.style.backgroundColor = b3color;
     d4.style.backgroundColor = b6color;
     d1.style.backgroundColor = b9color;
+    checkResult();
 }
 
 function uClicked() {
@@ -242,6 +252,7 @@ function uClicked() {
     b1.style.backgroundColor = l1color;
     b2.style.backgroundColor = l2color;
     b3.style.backgroundColor = l3color;
+    checkResult();
 }
 
 function uReverseClicked() {
@@ -295,6 +306,7 @@ function uReverseClicked() {
     l1.style.backgroundColor = b1color;
     l2.style.backgroundColor = b2color;
     l3.style.backgroundColor = b3color;
+    checkResult();
 }
 
 function fClicked() {
@@ -348,6 +360,7 @@ function fClicked() {
     d1.style.backgroundColor = r7color;
     d2.style.backgroundColor = r4color;
     d3.style.backgroundColor = r1color;
+    checkResult();
 }
 
 
@@ -402,6 +415,7 @@ function fReverseClicked() {
     r7.style.backgroundColor = d1color;
     r4.style.backgroundColor = d2color;
     r1.style.backgroundColor = d3color;
+    checkResult();
 }
 
 
@@ -456,6 +470,7 @@ function rClicked() {
     b1.style.backgroundColor = u9color;
     b4.style.backgroundColor = u6color;
     b7.style.backgroundColor = u3color;
+    checkResult();
 }
 
 
@@ -511,6 +526,7 @@ function rReverseClicked() {
     u9.style.backgroundColor = b1color;
     u6.style.backgroundColor = b4color;
     u3.style.backgroundColor = b7color;
+    checkResult();
 }
 
 
@@ -565,6 +581,7 @@ function bClicked() {
     l1.style.backgroundColor = u3color;
     l4.style.backgroundColor = u2color;
     l7.style.backgroundColor = u1color;
+    checkResult();
 }
 
 
@@ -619,6 +636,7 @@ function bReverseClicked() {
     u3.style.backgroundColor = l1color;
     u2.style.backgroundColor = l4color;
     u1.style.backgroundColor = l7color;
+    checkResult();
 }
 
 
@@ -673,6 +691,7 @@ function dClicked() {
     l7.style.backgroundColor = b7color;
     l8.style.backgroundColor = b8color;
     l9.style.backgroundColor = b9color;
+    checkResult();
 }
 
 
@@ -726,6 +745,7 @@ function dReverseClicked() {
     b7.style.backgroundColor = l7color;
     b8.style.backgroundColor = l8color;
     b9.style.backgroundColor = l9color;
+    checkResult();
 }
 
 function l2Clicked() {
@@ -907,14 +927,102 @@ function rotateDownClicked() {
     u2.style.backgroundColor = b8color;
 }
 
+function rotateLeft2dClicked() {
+    fReverseClicked();
+    bClicked();
+
+    var l2color = getColor(l2);
+    var l5color = getColor(l5);
+    var l8color = getColor(l8);
+
+    var u4color = getColor(u4);
+    var u5color = getColor(u5);
+    var u6color = getColor(u6);
+
+    var r2color = getColor(r2);
+    var r5color = getColor(r5);
+    var r8color = getColor(r8);
+
+    var d4color = getColor(d4);
+    var d5color = getColor(d5);
+    var d6color = getColor(d6);
+
+    l2.style.backgroundColor = u6color;
+    l5.style.backgroundColor = u5color;
+    l8.style.backgroundColor = u4color;
+
+    u4.style.backgroundColor = r2color;
+    u5.style.backgroundColor = r5color;
+    u6.style.backgroundColor = r8color;
+
+    r2.style.backgroundColor = d6color;
+    r5.style.backgroundColor = d5color;
+    r8.style.backgroundColor = d4color;
+
+    d4.style.backgroundColor = l2color;
+    d5.style.backgroundColor = l5color;
+    d6.style.backgroundColor = l8color;
+}
+
+function rotateRight2dClicked() {
+    fClicked();
+    bReverseClicked();
+
+    var l2color = getColor(l2);
+    var l5color = getColor(l5);
+    var l8color = getColor(l8);
+
+    var u4color = getColor(u4);
+    var u5color = getColor(u5);
+    var u6color = getColor(u6);
+
+    var r2color = getColor(r2);
+    var r5color = getColor(r5);
+    var r8color = getColor(r8);
+
+    var d4color = getColor(d4);
+    var d5color = getColor(d5);
+    var d6color = getColor(d6);
+
+    u6.style.backgroundColor = l2color;
+    u5.style.backgroundColor = l5color;
+    u4.style.backgroundColor = l8color;
+
+    r2.style.backgroundColor = u4color;
+    r5.style.backgroundColor = u5color;
+    r8.style.backgroundColor = u6color;
+
+    d6.style.backgroundColor = r2color;
+    d5.style.backgroundColor = r5color;
+    d4.style.backgroundColor = r8color;
+
+    l2.style.backgroundColor = d4color;
+    l5.style.backgroundColor = d5color;
+    l8.style.backgroundColor = d6color;
+    if (secretNumber > secretMaxNumber) {
+        if (isSecretHidden) {
+            showHiddenButtons();
+            isSecretHidden = false;
+        }
+    } else {
+        secretNumber++;
+    }
+}
+
 function scrambleClicked() {
     resetClicked();
 
+    isPlaying = true;
     var myAlgorithm = [];
 
+    var rotation = "X"
+
     for (var i = 1; i < 31; i++) {
-        var rotation = allRotations[Math.floor(Math.random() * allRotations.length)];
+        const candidateRotations = allRotations.filter(word => word[0] != [rotation[0]]);
+        rotation = candidateRotations[Math.floor(Math.random() * candidateRotations.length)];
         myAlgorithm.push(rotation);
+
+
         switch (rotation) {
             case "L":
                 lClicked();
@@ -976,11 +1084,12 @@ function scrambleClicked() {
 
 
     }
-    console.log(myAlgorithm.join("  "));
     algorithm.textContent = myAlgorithm.join("  ")
 }
 
 function resetClicked() {
+    isPlaying = false;
+
     algorithm.textContent = "–";
 
     l1.style.backgroundColor = "#FFA101";
@@ -1044,3 +1153,264 @@ function resetClicked() {
     d9.style.backgroundColor = "#F0FF19";
 
 }
+
+
+function checkboxClicked() {
+
+    var back = document.getElementById("back-side");
+    var down = document.getElementById("down-side");
+    var left = document.getElementById("left-side");
+    var dummy = document.getElementById("dummy-side");
+
+    back.classList.toggle('isHidden');
+    down.classList.toggle('isHidden');
+    left.classList.toggle('isHidden');
+    dummy.classList.toggle('isHidden');
+
+}
+
+function checkResult() {
+
+    if (!isPlaying) {
+        return;
+    }
+
+
+    // check upper side
+    var u1color = getColor(u1);
+    var u2color = getColor(u2);
+    var u3color = getColor(u3);
+    var u4color = getColor(u4);
+    var u5color = getColor(u5);
+    var u6color = getColor(u6);
+    var u7color = getColor(u7);
+    var u8color = getColor(u8);
+    var u9color = getColor(u9);
+    if (u5color == u1color
+        && u5color == u2color
+        && u5color == u3color
+        && u5color == u4color
+        && u5color == u6color
+        && u5color == u7color
+        && u5color == u8color
+        && u5color == u9color) {
+    } else {
+        return;
+
+    }
+
+    // check left side
+    var l1color = getColor(l1);
+    var l2color = getColor(l2);
+    var l3color = getColor(l3);
+    var l4color = getColor(l4);
+    var l5color = getColor(l5);
+    var l6color = getColor(l6);
+    var l7color = getColor(l7);
+    var l8color = getColor(l8);
+    var l9color = getColor(l9);
+    if (l5color == l1color
+        && l5color == l2color
+        && l5color == l3color
+        && l5color == l4color
+        && l5color == l6color
+        && l5color == l7color
+        && l5color == l8color
+        && l5color == l9color) {
+    } else {
+        return;
+
+    }
+
+    // check front side
+    var f1color = getColor(f1);
+    var f2color = getColor(f2);
+    var f3color = getColor(f3);
+    var f4color = getColor(f4);
+    var f5color = getColor(f5);
+    var f6color = getColor(f6);
+    var f7color = getColor(f7);
+    var f8color = getColor(f8);
+    var f9color = getColor(f9);
+    if (f5color == f1color
+        && f5color == f2color
+        && f5color == f3color
+        && f5color == f4color
+        && f5color == f6color
+        && f5color == f7color
+        && f5color == f8color
+        && f5color == f9color) {
+    } else {
+        return;
+
+    }
+
+    // check right side
+    var r1color = getColor(r1);
+    var r2color = getColor(r2);
+    var r3color = getColor(r3);
+    var r4color = getColor(r4);
+    var r5color = getColor(r5);
+    var r6color = getColor(r6);
+    var r7color = getColor(r7);
+    var r8color = getColor(r8);
+    var r9color = getColor(r9);
+    if (r5color == r1color
+        && r5color == r2color
+        && r5color == r3color
+        && r5color == r4color
+        && r5color == r6color
+        && r5color == r7color
+        && r5color == r8color
+        && r5color == r9color) {
+    } else {
+        return;
+
+    }
+
+    // check back side
+    var b1color = getColor(b1);
+    var b2color = getColor(b2);
+    var b3color = getColor(b3);
+    var b4color = getColor(b4);
+    var b5color = getColor(b5);
+    var b6color = getColor(b6);
+    var b7color = getColor(b7);
+    var b8color = getColor(b8);
+    var b9color = getColor(b9);
+    if (b5color == b1color
+        && b5color == b2color
+        && b5color == b3color
+        && b5color == b4color
+        && b5color == b6color
+        && b5color == b7color
+        && b5color == b8color
+        && b5color == b9color) {
+    } else {
+        return;
+
+    }
+
+    // check down side
+    var d1color = getColor(d1);
+    var d2color = getColor(d2);
+    var d3color = getColor(d3);
+    var d4color = getColor(d4);
+    var d5color = getColor(d5);
+    var d6color = getColor(d6);
+    var d7color = getColor(d7);
+    var d8color = getColor(d8);
+    var d9color = getColor(d9);
+    if (d5color == d1color
+        && d5color == d2color
+        && d5color == d3color
+        && d5color == d4color
+        && d5color == d6color
+        && d5color == d7color
+        && d5color == d8color
+        && d5color == d9color) {
+        isPlaying = false;
+        setTimeout(showWin, 300);
+
+    } else {
+        return;
+
+    }
+}
+
+function showWin() {
+    window.alert("Congratulations! You did it!")
+    if (isSecretHidden) {
+        isSecretHidden = false
+        showHiddenButtons();
+
+    }
+}
+
+function showHiddenButtons() {
+    var secretButtons = document.getElementById("secret-buttons");
+    secretButtons.classList.toggle('isHidden');
+
+}
+
+function secret1Clicked() {
+    uClicked();
+    rClicked();
+    uReverseClicked();
+    rReverseClicked();
+}
+
+function secret2Clicked() {
+    rClicked();
+    uClicked();
+    rReverseClicked();
+    uReverseClicked();
+}
+
+
+function secret3Clicked() {
+    uReverseClicked();
+    lReverseClicked();
+    uClicked();
+    lClicked();
+    uClicked();
+    fClicked();
+    uReverseClicked();
+    fReverseClicked();
+}
+
+function secret4Clicked() {
+    uClicked();
+    rClicked();
+    uReverseClicked();
+    rReverseClicked();
+    uReverseClicked();
+    fReverseClicked();
+    uClicked();
+    fClicked();
+}
+
+function secret5Clicked() {
+    fClicked();
+    rClicked();
+    uClicked();
+    rReverseClicked();
+    uReverseClicked();
+    fReverseClicked();
+}
+
+function secret6Clicked() {
+    uReverseClicked();
+    rClicked();
+    uClicked();
+    uClicked();
+    rReverseClicked();
+    uReverseClicked();
+    rClicked();
+    uReverseClicked();
+    rReverseClicked();
+}
+
+function secret7Clicked() {
+    uReverseClicked();
+    lReverseClicked();
+    uClicked();
+    rClicked();
+    uReverseClicked();
+    lClicked();
+    uClicked();
+    rReverseClicked();
+}
+
+function secret8Clicked() {
+    uClicked();
+    rClicked();
+    uReverseClicked();
+    lReverseClicked();
+    uClicked();
+    rReverseClicked();
+    uReverseClicked();
+    lClicked();
+}
+
+
